c2caae1087a3889bf4eefeb1bc19fcba79f56cad
Gabriel Schumacker <42940546+gabrielSchumacker@users.noreply.github.com>
2021-03-17 21:56:35 -0300

    Feita a implementação da poda AB

--- Trabalho_2_poda_alpha_beta/reversIA/reversIA.py
+++ Trabalho_2_poda_alpha_beta/reversIA/reversIA.py
@@ -13,43 +13,89 @@ class Node(object):
         self.movimentos = []
         self.caminho = []
         self.oponente=estado.opponent(self.cor)
-	
-	self.pontos=calcula_pontos(self)	
+        #variavel para impor limite de profundidade
+        self.iter = 3
 
-    def add_movimentos(self):
-	#Usar funcoes definidas no board para executar os movimentos de movimentos
-	#Nao tenho ctz se aqui vai o oponente msm, mas faz sentido os proximos movimentos serem do oposto
-	coordenada_movimentos=self.estado.legal_moves(self.oponente)
-	#Criar a lista de estados a partir da lista de movimentos, instanciar novos nodos e colocar na arvore
-	#self.movimentos = 
-        self.move.movimentos(new_node)
+#    def add_movimentos(self):
+        #Usar funcoes definidas no board para executar os movimentos de movimentos
+        #Nao tenho ctz se aqui vai o oponente msm, mas faz sentido os proximos movimentos serem do oposto
+#       coordenada_movimentos=self.estado.legal_moves(self.oponente)
+        #Criar a lista de estados a partir da lista de movimentos, instanciar novos nodos e colocar na arvore
+        #self.movimentos = 
+#        self.move.movimentos(new_node)
 
-    def __str__(self):
-        return str(self.estado) + ": " + str(self.custo)
+#    def __str__(self):
+#        return str(self.estado) + ": " + str(self.custo)
 
     def make_move(self):
-        """
-        Returns a random move from the list of possible ones
-        Primeiro elemento eh o numero da coluna e o segunda da linha [0,7]
-        :return: (int, int)
-        """       
-        return random.choice(self.movimentos) if len(self.movimentos) > 0 else (-1, -1)
 
-    def encontrar_pecas(self):
+        v_atual = -999999
+        jogada_max = (-1, -1)
+        movimentos_legais = self.estado.legal_moves(self.cor)
+		
+        if(len(movimentos_legais) > 0):
+            for i in movimentos_legais:
+                tabuleiro = self.estado
+                tabuleiro.process_move(i,self.cor)
+                v = self.valorMAX(tabuleiro,-999999,999999,1)
+                if(v_atual < v):   
+                    jogada_max = i
+                    v_atual = v
+            return jogada_max
+        else:
+            return (-1, -1)
+
+    def valorMAX(self,tabuleiro,alpha,beta,iter):
+
+        movimentos_legais = self.estado.legal_moves(self.oponente)
+        if(iter == self.iter or len(movimentos_legais) == 0):
+            self.calcula_pontos(tabuleiro)
+            utilidade = self.pontos
+            self.pontos = 0
+            return utilidade
+        else:
+            for i in movimentos_legais:
+                tabuleiro_novo = tabuleiro
+                tabuleiro_novo.process_move(i,self.oponente)
+                v = self.valorMIN(tabuleiro_novo,alpha,beta,iter+1)
+                alpha_novo = max(alpha,v)
+                if(beta < alpha_novo):
+                    return alpha_novo
+            return alpha
+
+    def valorMIN(self,tabuleiro,alpha,beta,iter):
+
+        movimentos_legais = self.estado.legal_moves(self.cor)
+        if(iter == self.iter or len(movimentos_legais) == 0):
+            self.calcula_pontos(tabuleiro)
+            utilidade = self.pontos
+            self.pontos = 0
+            return utilidade
+        else:
+            for i in movimentos_legais:
+                tabuleiro_novo = tabuleiro
+                tabuleiro_novo.process_move(i,self.cor)
+                v = self.valorMAX(tabuleiro_novo,alpha,beta,iter+1)
+                beta_novo = min(beta,v)
+                if(alpha > beta_novo):
+                    return beta_novo
+            return beta
+
+    def encontrar_pecas(self,tabuleiro):
         """
         Retorna uma lista com a posicao das pecas em
         '........\n........\n........\n........\n........\n........\n........\n........\n'
         :return: [[aliada],[inimigas]]
         """
         aliadas=[]
-        tabuleiroStr=str(self.estado)
+        tabuleiroStr=str(tabuleiro)
         for charPos in range(len(tabuleiroStr)):
             if(tabuleiroStr[charPos]==self.cor):
                 aliadas.append(charPos)
         return aliadas
 
-    def calcula_pontos(self):
-        aliadas= self.encontrar_pecas()
+    def calcula_pontos(self,tabuleiro):
+        aliadas= self.encontrar_pecas(tabuleiro)
 
         #Nao fazer um alg guloso pela captura
         self.pontos = self.pontos + 1*len(aliadas)
@@ -63,10 +109,10 @@ class Node(object):
         quinas=[0, 7, 63, 71]
         for pos in aliadas:
             for i in range(4):
-            	if(quinas[i] in aliadas):
-            		#remove as 3 posicoes do C adjacente a quina preenchida
-            		adjquina=list(set(adjquina)-set(adjquina_fixa[i*3:(i*3+4)]))
-	                self.pontos=self.pontos+20
+                if(quinas[i] in aliadas):
+                    #remove as 3 posicoes do C adjacente a quina preenchida
+                    adjquina=list(set(adjquina)-set(adjquina_fixa[i*3:(i*3+4)]))
+                    self.pontos=self.pontos+20
 
         # -20 pela regiao adjacente a quina (borda C)        
         for pos in aliadas:
@@ -86,7 +132,7 @@ class Node(object):
                 self.pontos = self.pontos + 10
 
         #-5 pts pela regiao adjacente as bordas centrais
-	adj_bordas_centrais = [12,13,28,37,33,42,57,58]
+        adj_bordas_centrais = [12,13,28,37,33,42,57,58]
         for pos in aliadas:
             if (pos in adj_bordas_centrais):
                 self.pontos = self.pontos - 5
